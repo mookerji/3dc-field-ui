47635ad008d0e7c6d7a77dbfec9687e623cca15d
Buro Mookerji <buro.mookerji@mapbox.com>
2020-04-07 22:23:26 -0700

    Load on page.

---
+++ main.js
@@ -0,0 +1,26 @@
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoibWFza3Nvbi1pbnRlcm5hbCIsImEiOiJjazhxdTNjOXAwN3NsM2RwaDJkaWU1MGV4In0.91Sfq6oHaoP-kD6fV-shFA";
+
+mapboxgl.accessToken = MAPBOX_TOKEN;
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-71.0942, 42.3601],
+  zoom: 9,
+});
+
+map.on("load", function () {
+  init();
+});
+
+map.addControl(
+  new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl,
+  })
+);
+
+function init() {
+  console.log("loaded!");
+}
