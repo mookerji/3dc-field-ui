1c46f2194dc5db3370e14df80bff893e4e79154d
Buro Mookerji <buro.mookerji@mapbox.com>
2020-04-08 00:15:37 -0700

    Hover

--- main.js
+++ main.js
@@ -5,12 +5,16 @@ mapboxgl.accessToken = MAPBOX_TOKEN;
 
 const map = new mapboxgl.Map({
   container: "map",
-    //  style: "mapbox://styles/maskson-internal/ck8qye4uf059t1ipsdcdtcy90",
+  //  style: "mapbox://styles/maskson-internal/ck8qye4uf059t1ipsdcdtcy90",
   style: "mapbox://styles/mapbox/streets-v11",
-  center: [-122.4639339,37.7758159],
-  zoom: 7,
+  center: [-122.4639339, 37.7758159],
+  zoom: 11,
 });
 
+const popup = new mapboxgl.Popup({
+  closeButton: false,
+  closeOnClick: false,
+});
 
 map.on("load", function () {
   init();
@@ -23,6 +27,41 @@ map.addControl(
   })
 );
 
+let lastId;
+
+map.on("mousemove", "circles", (e) => {
+  const id = e.features[0].properties.name;
+  if (id !== lastId) {
+    lastId = id;
+    map.getCanvas().style.cursor = "pointer";
+    const { name, vicinity } = e.features[0].properties;
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const HTML = `<p><b>Name</b>: ${name}<br><b/>Address</b>: ${vicinity}</p>`;
+    popup.setLngLat(coordinates).setHTML(HTML).addTo(map);
+  }
+});
+
+map.on("mouseleave", "circles", function () {
+  lastId = undefined;
+  map.getCanvas().style.cursor = "";
+  popup.remove();
+});
+
 function init() {
-  console.log("loaded!");
+  map.addSource("demand", {
+    type: "geojson",
+    data:
+      "https://raw.githubusercontent.com/mookerji/3dc-field-ui/master/demand.json",
+  });
+  map.addLayer({
+    id: "circles",
+    source: "demand",
+    type: "circle",
+    paint: {
+      "circle-opacity": 0.75,
+      "circle-stroke-width": 1,
+      "circle-radius": 6,
+      "circle-color": "#FFEB3B",
+    },
+  });
 }
