3a49a9b274b08c507fef2b3a3388c12b93103e11
Buro Mookerji <buro.mookerji@mapbox.com>
2020-04-08 23:10:29 -0700

    Updated

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const MAPBOX_TOKEN =
 
 mapboxgl.accessToken = MAPBOX_TOKEN;
 
+if (!mapboxgl.supported()) {
+  alert("Your browser does not support Mapbox GL");
+}
+
 const map = new mapboxgl.Map({
   container: "map",
   //  style: "mapbox://styles/maskson-internal/ck8qye4uf059t1ipsdcdtcy90",
@@ -10,6 +14,8 @@ const map = new mapboxgl.Map({
   center: [-122.4639339, 37.7758159],
   zoom: 11,
 });
+
+const filterGroup = document.getElementById("filter-group");
 
 const popup = new mapboxgl.Popup({
   closeButton: false,
@@ -20,32 +26,57 @@ map.on("load", function () {
   init();
 });
 
-map.addControl(
-  new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken,
-    mapboxgl: mapboxgl,
-  })
-);
-
 let lastId;
 
-map.on("mousemove", "circles", (e) => {
+function recipientToHTML(properties) {
+  const {
+    name,
+    vicinity,
+    contact_name = "n/a",
+    contact_email = "n/a",
+    created_at = "n/a",
+    status = "n/a",
+  } = properties;
+  return `<p><b>Name</b>: ${name}<br><b/>Address</b>: ${vicinity}<br><b/>Contact</b>: ${contact_name}<br><b/>Email</b>: ${contact_email}<br><b/>Created At</b>: ${created_at}<br><b/>Status</b>: ${status}</p>`;
+}
+
+map.on("mousemove", "demand", (e) => {
   const id = e.features[0].properties.name;
   if (id !== lastId) {
     lastId = id;
     map.getCanvas().style.cursor = "pointer";
-    const { name, vicinity } = e.features[0].properties;
     const coordinates = e.features[0].geometry.coordinates.slice();
-    const HTML = `<p><b>Name</b>: ${name}<br><b/>Address</b>: ${vicinity}</p>`;
+    const HTML = recipientToHTML(e.features[0].properties);
     popup.setLngLat(coordinates).setHTML(HTML).addTo(map);
   }
 });
 
-map.on("mouseleave", "circles", function () {
+map.on("mouseleave", "demand", function () {
   lastId = undefined;
   map.getCanvas().style.cursor = "";
   popup.remove();
 });
+
+function addLayerSelect(layer_id) {
+  var input = document.createElement("input");
+  input.type = "checkbox";
+  input.id = layer_id;
+  input.checked = true;
+  filterGroup.appendChild(input);
+
+  var label = document.createElement("label");
+  label.setAttribute("for", layer_id);
+  label.textContent = layer_id;
+  filterGroup.appendChild(label);
+
+  input.addEventListener("change", function (e) {
+    map.setLayoutProperty(
+      layer_id,
+      "visibility",
+      e.target.checked ? "visible" : "none"
+    );
+  });
+}
 
 function init() {
   map.addSource("demand", {
@@ -54,14 +85,15 @@ function init() {
       "https://raw.githubusercontent.com/mookerji/3dc-field-ui/master/demand.json",
   });
   map.addLayer({
-    id: "circles",
+    id: "demand",
     source: "demand",
     type: "circle",
     paint: {
       "circle-opacity": 0.75,
       "circle-stroke-width": 1,
       "circle-radius": 6,
-      "circle-color": "#FFEB3B",
+      "circle-color": "#B42222", // Red
     },
   });
+  addLayerSelect("demand");
 }
